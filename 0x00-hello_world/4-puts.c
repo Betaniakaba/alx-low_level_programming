9927e4f10d567297c61d02af1c8b73cdd664fb36
Betania <betania.berhanu@outlook.com>
2022-09-08 12:48:39 -0700

    print using put function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
-#include <stdio.h>
-
+# include <stdio.h>
+ 
 /**
-* main - print using the put function
-*
-* Description: use the main function
-* a C program that prints "Programming is like building a multilingual puzzle"
-* Return : 0
-*/
+ * main - print using the put function
+ *
+ * Description: use the main function
+ * a C program that prints "Programming is like building a multilingual puzzle"
+ * Return: 0
+ */
 int main (void)
 {
-  puts ("\"Programming is like building a multilingual puzzle");
-  Return (0);
-}
+    puts("\"Programming is like building a multilingual puzzle"
+	    Return (0);
+	    }	    	    
