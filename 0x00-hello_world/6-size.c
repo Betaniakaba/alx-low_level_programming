c03ba35c4277e540fbb4b759fd160acd63ee3251
Betania <betania.berhanu@outlook.com>
2022-09-09 00:30:50 -0700

    8

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,5 +16,5 @@ printf("Size of a int: %d byte(s)\n", (unsigned long)sizeof(a));
 printf("Size of a long int: %d byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of a long long int: %d byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of a float: %d byte(s)\n", (unsigned long)sizeof(f));
-return(0);
+return (0);
 }
