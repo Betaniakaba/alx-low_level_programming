fbba730779d5b5291a11bac930d66ad4daa67135
Betania <betania.berhanu@outlook.com>
2022-09-09 00:56:20 -0700

    98

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,15 +6,15 @@
  */
 int main(void)
 {
-int a;
-long int b;
-long long int c;
-char d;
+char c;
+int i;
+long int l;
+long long int d;
 float f;
-printf("Size of a char: %d byte(s)\n", (unsigned long)sizeof(d));
-printf("Size of a int: %d byte(s)\n", (unsigned long)sizeof(a));
-printf("Size of a long int: %d byte(s)\n", (unsigned long)sizeof(b));
-printf("Size of a long long int: %d byte(s)\n", (unsigned long)sizeof(c));
-printf("Size of a float: %d byte(s)\n", (unsigned long)sizeof(f));
+printf("Size of a char: %ld byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a int: %ld byte(s)\n", (unsigned long)sizeof(i));
+printf("Size of a long int: %ld byte(s)\n", (unsigned long)sizeof(l));
+printf("Size of a long long int: %ld byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of a float: %ld byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
