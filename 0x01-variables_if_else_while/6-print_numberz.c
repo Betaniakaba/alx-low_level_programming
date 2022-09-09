843979e5d1d65f8449e0da96bb9e13ca5b21cdd7
Betania <betania.berhanu@outlook.com>
2022-09-09 14:31:38 -0700

    7th Task

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* code description
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int a = 48;
+
+while (a < 58)
+{
+putchar(a);
+a++;
+}
+putchar('\n');
+return (0);
+}
