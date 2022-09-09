d423b9a459527fbc53589ab950448fbe10ae40db
Betania <betania.berhanu@outlook.com>
2022-09-08 22:08:44 -0700

    4th Task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
 *main - print the string in the puts function
-*Description: using the main function
-*this program prints"Programming is like building a multilingual puzzle"
 *Return: 0
 */
 int main(void)
 { 
 puts("\"Programming is like building a multilingual puzzle");
-return (0);}
+return (0);
+}
