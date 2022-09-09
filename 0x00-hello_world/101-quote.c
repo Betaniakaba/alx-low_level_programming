f659fd844cf831ad8bca446bf07a8b56f7fd8109
Betania <betania.berhanu@outlook.com>
2022-09-09 02:09:35 -0700

    ty

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <unistd.h>
 /**
- * main - Entry block
+ * main - prints the standard error sandwiched around the outpu/t
   *
-   * Return: Always 0 (Sucess)
-   * 
+   * Return: returns 1
  */
 int main(void)
 {
-write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19", 59);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return(1);
 }
