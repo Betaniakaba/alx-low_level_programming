627531c2a7c5b1b336143be5c1eb0cfd7706c3bf
Betania <betania.berhanu@outlook.com>
2022-09-09 02:03:53 -0700

    t

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - Entry block
- *
- * Return: Always 0 (Sucess)
+  *
+   * Return: Always 0 (Sucess)
  */
 int main(void)
 {
-fwrite(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19", 61);
+write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19", 61);
 return(1);
 }
