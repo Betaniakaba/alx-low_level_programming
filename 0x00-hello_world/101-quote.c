584ebc0f45b1574353d7baad2d7d725d5fde50c2
Betania <betania.berhanu@outlook.com>
2022-09-08 22:49:12 -0700

    8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry block
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19", 61);
+return(1);
+}
