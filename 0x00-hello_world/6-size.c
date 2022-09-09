8b9b90034bdaa51036d2d4b292d54b0659446a9e
Betania <betania.berhanu@outlook.com>
2022-09-09 02:00:32 -0700

    y

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - print sizes
- *
- * Return: 0
+ * main - Entry point
+  *
+   * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -13,7 +13,7 @@ char d;
 float f;
 
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
-printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
