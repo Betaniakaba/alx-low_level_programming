9fec4d24317434d001572d788473226df04e7816
Betania <betania.berhanu@outlook.com>
2022-09-09 02:06:29 -0700

    ty

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry block
   *
    * Return: Always 0 (Sucess)
+   * 
  */
 int main(void)
 {
-write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19", 61);
+write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19", 59);
 return(1);
 }
