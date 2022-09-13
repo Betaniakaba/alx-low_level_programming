005b78b7553c8be193f04ddfd05eebec830b6b3d
Betania <betania.berhanu@outlook.com>
2022-09-13 13:33:32 -0700

    2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 /**
-* print_alpabet - Entry Point
-* 
+* print_alphabet - Entry Point
+*
 * Return: Always 0
 */
 int print_alphabet(void)
 {
 char b;
-for(b = 'a'; b <= 'z'; b++)
+for (b = 'a'; b <= 'z'; b++)
 {
 putchar(b);
 }
 putchar('\n');
-return 0;
+return (0);
 }
