28da7235b659cd0c4a7bc0c7f2d7e3b682554b7f
Betania <betania.berhanu@outlook.com>
2022-09-13 11:37:40 -0700

    2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* print_alphabet - Entry Point
+* 
+* Description : Print the alphabet in lowercase
+* Return: Always 0
+*/
+int main(void)
+{
+void print_alphabet(void)
+{
+Char b;
+for(b = 'a'; b <= 'z'; b++)
+{
+_putchar(b);
+}
+_putchar('\n');
+return(0);
+}
