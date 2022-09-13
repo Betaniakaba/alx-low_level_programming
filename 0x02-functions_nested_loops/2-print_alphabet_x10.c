f810d674c9ca8f538613286ae7ac37fd7f4296b4
Betania <betania.berhanu@outlook.com>
2022-09-13 14:14:13 -0700

    3

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* print_alphabet_x10 - Entry Point
+*
+* Return: Always 0
+*/
+int print_alphabet_x10(void)
+{
+char b;
+int e = 0;
+while (e <= 9)
+{
+for (b = 'a'; b <= 'z'; b++)
+{
+putchar(b);
+}
+putchar('\n');
+e++;
+}
+return (0);
+}
