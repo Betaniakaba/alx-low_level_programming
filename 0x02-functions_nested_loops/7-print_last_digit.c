15064d9013666e9d0dcca4bcf59f56ec49961cba
Betania <betania.berhanu@outlook.com>
2022-09-14 12:03:04 -0700

    8th

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_last_digit - function that prints the last digit of a number
+* @b: is the int used for the computation
+* 
+* Return: Value of the last digit
+*/
+int print_last_digit(int e);
+{
+int b = e % 10;
+if (b < 0)
+b *= -1;
+_putchar(b + '0');
+return (b);
+}
