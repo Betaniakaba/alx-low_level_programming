b05a4c42d9dedad4e3e981b55ace7e2e4ec5371e
Betania <betania.berhanu@outlook.com>
2022-09-14 10:52:20 -0700

    sth

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,14 +11,15 @@ if (n > 0)
 _putchar('+');
 return (1);
 }
-else if (n > 0)
-{
-_putchar('-')
-return (-1);
-}
-else
+else if (n == 0)
 {
 _putchar('0');
 return (0);
 }
+else
 }
+_putchar('-');
+return (-1);
+}
+}
+
