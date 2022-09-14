b60f087af5f08d8580ca92afa95720604de3e77d
Betania <betania.berhanu@outlook.com>
2022-09-14 10:45:32 -0700

    5th

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* print_sign - a function that prints the sign of a number
+* @n: is the int used to implement the argument
+* Return: 0
+*/
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n > 0)
+{
+_putchar('-');
+return (-1);
+}
+else
+{
+_putchar('0');
+return (0);
+}
+}
