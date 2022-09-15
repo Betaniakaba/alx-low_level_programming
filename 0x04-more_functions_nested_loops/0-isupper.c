40bbce2b0104b90d14badcec764f98ff161f976c
Betania <betania.berhanu@outlook.com>
2022-09-15 11:13:40 -0700

    1

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,39 +1,29 @@
 #include "main.h"
-
 #include <stdio.h>
-
-
 
 /**
  *
- * * _isupper - function that checks for uppercase character
+ * _isupper - function that checks for uppercase character
  *
- * * @c: will be the integer used for compiling
+ * @c: will be the integer used for compiling
  *
- * *
  *
- * * Return:0
  *
- * */
+ * Return:0
+ *
+ */
 
-int _isupper(int c);
+int _isupper(int c)
 
 {
+	char c;
 
-		char c;
+	c = 'C';
+	printf("Return value if uppercase character %c is passed to _isupper(): %d", c, _isupper(c));
 
-			c = 'C';
-
-				printf("Return value if uppercase character %c is passed to _isupper(): %d", c, _isupper(c));
-
-					
-
-					c = '+'
-
-							printf("Return value if another character %c is passed to _isupper(): %d", c, _isupper(c));
-
-
-
-						return 0;
+	c = '+';
+	printf("Return value if another character %c is passed to _isupper(): %d", c, _isupper(c));
+	
+	return 0;
 
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,2 +1,1 @@
 int _isupper(int c);
-int _isdigit(int c);
