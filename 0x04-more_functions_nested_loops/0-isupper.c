1f6c96c3532cbf473ee2169127002626f4a4afd5
Betania <betania.berhanu@outlook.com>
2022-09-15 11:32:02 -0700

    1

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,29 +1,24 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- *
- * _isupper - function that checks for uppercase character
- *
- * @c: will be the integer used for compiling
- *
- *
- *
- * Return:0
- *
- */
-
-int _isupper(int c)
+*
+* _isupper - function that checks for upeercase characters
+* @c: the integer used for compilation
+* 
+* Return: 1
+*/
+int _isupper(int c);
 
 {
-	char c;
 
-	c = 'C';
-	printf("Return value if uppercase character %c is passed to _isupper(): %d", c, _isupper(c));
+if ((c >= 'A') && (c <= 'Z'))
 
-	c = '+';
-	printf("Return value if another character %c is passed to _isupper(): %d", c, _isupper(c));
-	
-	return 0;
+	{
+
+	return (1);
+
+	}
+
+return (0);
 
 }
