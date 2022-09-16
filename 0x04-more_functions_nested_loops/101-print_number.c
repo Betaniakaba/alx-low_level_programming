91045c9da5710e8fd178c20fa628323694604152
Betania <betania.berhanu@outlook.com>
2022-09-16 11:34:16 -0700

    uiiii

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,36 +1,22 @@
 #include "main.h"
 
 
-
-/**
-
+/*
  * print_number -  printing an integer
-
  * @n: n -  Variable
-
  * Return: Always 0.
-
  */
 
 void print_number(int n)
-
 {
-
 	unsigned int z;
-
 	int m, b;
-
-
 
 	b = 10;
 
-
-
 	if (n < 10 && n >= 0)
-
 	{
-
-		_putchar (n + '0');
+		_putchar(n + '0');
 
 	}
 
@@ -42,52 +28,29 @@ void print_number(int n)
 
 		_putchar('-');
 
-		_putchar (n + '0');
+		_putchar(n + '0');
 
 	}
-
-
-
 	else
-
 	{
-
 		if (n < 0)
-
 		{
-
 			n = n * -1;
-
-			_putchar ('-');
-
+			_putchar('-');
 		}
-
 		z = n;
-
 	while (z / b > 9)
-
 	{
-
 		b = b * 10;
-
 	}
-
 	while (b > 0)
-
 	{
-
 		m = z / b;
-
 		z = z % b;
-
 		_putchar (m + '0');
-
 		b = b / 10;
-
 	}
-
 	}
-
 }
 
 
