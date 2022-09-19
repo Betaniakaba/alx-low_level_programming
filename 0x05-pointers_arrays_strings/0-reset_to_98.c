be187376cfb3fde01eb06a32a0033817b4b8c96f
Betania <betania.berhanu@outlook.com>
2022-09-19 11:01:51 -0700

    Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * reset_to_98 - a function that takes a pointer to an int as parameter and updates the value it points to to 98
+ * reset_to_98 - a function that takes a pointer value 98
  * @n: serves as the pointer to reset to 98
  * Return: void
  */
