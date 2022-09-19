b41c118d8ec681a42e405e99fde6d798eba25421
Betania <betania.berhanu@outlook.com>
2022-09-19 14:31:35 -0700

    0T

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - a function that prints in reverse
+ * @s: integer used
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int a = 0;
+
+	while (*(str + a))
+	{
+	_putchar(*(str + a));
+	a--;
+	}
+	_putchar('\n');
+}
