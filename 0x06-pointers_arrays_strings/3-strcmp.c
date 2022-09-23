c38a12830866c6cad20ef778b8a86d1214b88a7c
Betania <betania.berhanu@outlook.com>
2022-09-23 11:28:18 -0700

    Compare 2 strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,15 +10,29 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, op = 0;
+	int i = 0, comp = 0;
 
-	while (op == 0)
+	while (1)
 	{
-		if (((s1 + i) == '\0') && ((s2 + i) == '\0'))
+		if (s1[i] == '\0' && s2[i] == '\0')
 			break;
-		op = *(s1 + i) - *(s2 + i);
-		i++;
+		else if (s1[i] == '\0')
+		{
+			comp = s2[i];
+			break;
+		}
+		else if (s2[i] == '\0')
+		{
+			comp = s1[i];
+			break;
+		}
+		else if (s1[i] != s2[i])
+		{
+			comp = s1[i] - s2[i];
+			break;
+		}
+		else
+			i++;
 	}
-
-	return (op);
+	return (comp);
 }
