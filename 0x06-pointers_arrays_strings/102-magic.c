b38a4852ddc53f18d25c91883d2de9929ba9db3f
Betania <betania.berhanu@outlook.com>
2022-09-23 11:39:42 -0700

    Magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
 
-
 int main(void)
 {
 	int n;
+
 	int a[5];
 	int *p;
-
 	a[2] = 1024;
 	p = &n;
-/**
- * main - magic 
- * @p: char used
+/*
+ * main - magic reference
+ * write your line of code here...
+ * - you are not allowed to use a
+ *   - you are not allowed to modify
+ *   - only oen statement
+ *   - you are not allowed to code anythign else than this line of code
+ *   Return: Void
  */
-	*(p + 5) = 98;
-/* ...so that this prints 98\n */
+	*(p+5) = 98
+/*  that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
