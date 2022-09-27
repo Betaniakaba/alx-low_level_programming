b15616cd220f58cd549fab5459a6013337a579c4
Betania <betania.berhanu@outlook.com>
2022-09-27 15:08:46 -0700

    find the first occurrence

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h
+
+/**
+* _strstr - locates a substring
+* @needle: substring
+* @haystack: entire string
+* Return: pointer to the beginning needle
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	char *a;
+	char *b;
+
+	while (*haystack != '\0')
+	{
+	a = haystack;
+	b = needle;
+
+	while- (*haystack != '\0' && *b != '\0' && *haystack == *b)
+	{
+	haystack++;
+	b++;
+	}
+	if (!*b)
+	return (a);
+	haystack = a +1;
+}
+return (NULL);
+}
