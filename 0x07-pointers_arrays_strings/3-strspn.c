b779195a50a7eb06f132c0c5157fb3f7839de641
Betania <betania.berhanu@outlook.com>
2022-09-27 14:45:47 -0700

    get length of string

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* _strspn - get the length of a prefix substring
+* @s: The initial segment
+* @accept: accepted bytes
+* Return: number of bytes in the initial segment
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+	int b, e, t;
+
+	for (b = 0; *(s + b) != '\0'; b++)
+	{
+		t = 1;
+		for (e = 0; *(accept + e) != '\0'; e++)
+	{
+	if (*(s + b) == *(accept + e))
+	{
+	t = 0;
+	break;
+		}
+	}
+	if (t == 1)
+	break;
+	}
+	return (b);
+}
