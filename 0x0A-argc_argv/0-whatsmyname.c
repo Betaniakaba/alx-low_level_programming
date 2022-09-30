588ffed4eac66d1440566d6e7f2a6690eee919b1
Betania <betania.berhanu@outlook.com>
2022-09-30 12:33:58 -0700

    Print name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - prints name followed by a new line
+* @argc: this is the argument count
+* @argv: this is the argument vector
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
