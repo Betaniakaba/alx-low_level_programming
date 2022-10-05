e97618f130db1a9cac756acea51fb30135141637
Betania <betania.berhanu@outlook.com>
2022-10-05 12:28:29 -0700

    Second Task

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - copies the string given as parameter
+ * @str: string to duplicate
+ *
+ * Return: pointer to the copied string (Success), NULL (Error)
+ */
+char *_strdup(char *str)
+{
+	char *bet;
+	unsigned int i, len;
+
+	i = 0;
+	len = 0;
+	if (str == NULL)
+		return (NULL);
+	while (str[len])
+		len++;
+	bet = malloc(sizeof(char) * (len + 1));
+	if (bet == NULL)
+		return (NULL);
+	while ((bet[i] = str[i]) != '\0')
+		i++;
+	return (bet);
+}
