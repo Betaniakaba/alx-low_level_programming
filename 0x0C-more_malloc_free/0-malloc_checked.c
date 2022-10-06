fe96713fc31e9e1b753094866270835102c34ec7
Betania <betania.berhanu@outlook.com>
2022-10-06 11:51:54 -0700

    Memory Allocation

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -12,9 +12,9 @@ void *malloc_checked(unsigned int b)
 	char *c;
 /* allocation of malloc(b) to c*/
 	c = malloc(b);
-/* exit function calls all functions registered with atexit and terminates the program*/
+/* exit fn calls all fns registered with atexit and terminates the program*/
 	if (c == NULL)
-/* atexit fn registers a fn as a termination fn wc is called if the program terminates normally*/
+/* atexit fn registers as a trmntn fn wc is called if the program terminates */
 		exit(98);
 	return (c);
 }
