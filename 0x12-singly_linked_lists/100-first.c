03d58d650d3adb807280199ccd0f8096b1b74d6a
Betania <betania.berhanu@outlook.com>
2022-10-20 16:43:47 -0700

    Singly linked lists

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
 
+
+
 /**
+
  * bmain - function executed before main
+
  * Return: no return.
+
  */
 
-void _attribute_ ((constructor)) bmain()
+
+
+void__attribute__((constructor)) bmain()
+
 {
+
 	printf("You're beat! and yet, you must allow");
+
 	printf(",\nI bore my house upon my back!\n");
+
 }
